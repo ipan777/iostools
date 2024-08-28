4b22956524166ccbb9bd20634bbb710e091e70f5
ipan777 <ipan@duck.com>
2024-08-28 08:41:22 +0800

    Update zxyhNoad.js

--- zxyhNoad.js
+++ zxyhNoad.js
@@ -1,3 +1,3 @@
 # 中信银行去广告 (终身躺平版)
 ^https:\/\/imcs\.citicbank\.com\/cloud\/([a-fA-F0-9]{32})\.(jpg|jpeg|png)\?width=[0-9]{4}&height=[0-9]{4} url reject
-MitM =imcs.citicbank.com
+hostname =imcs.citicbank.com
