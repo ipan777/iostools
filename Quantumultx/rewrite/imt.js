1725b30348ac08f37ba60d11601c04f8fb229185
ipan777 <ipan@duck.com>
2024-10-18 10:02:30 +0800

    Update imt.js

--- Quantumultx/rewrite/imt.js
+++ Quantumultx/rewrite/imt.js
@@ -7,11 +7,11 @@
 hostname = app.moutai519.com.cn
 
 [rewrite_local]
-https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/ipan777/crack-iOS-app/main/imt.js
+https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/ipan777/iostools/refs/heads/main/Quantumultx/rewrite/imt.js
 
 [task_local]
 # 茅台自动预约
-0 9 * * * https://raw.githubusercontent.com/ipan777/crack-iOS-app/main/imt.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
+0 9 * * * https://raw.githubusercontent.com/ipan777/iostools/refs/heads/main/Quantumultx/rewrite/imt.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
 ```
 ******************************************/
 var $ = new Env('i茅台'),service = $.http
